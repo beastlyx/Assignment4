f66302054e5dfb6582a34d16fb147dc7581bad93
beastlyx <91150232+beastlyx@users.noreply.github.com>
2024-04-17 20:09:03 -0500

    Initial commit

---
+++ src/main/java/ui/Connect4GUI.java
@@ -0,0 +1,12 @@
+package ui;
+
+/**
+ *
+ * @author borysb
+ */
+public class Connect4GUI {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
